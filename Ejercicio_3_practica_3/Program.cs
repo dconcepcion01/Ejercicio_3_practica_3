9dabbae35063b97f1af9a3451f7a9cf14e60fe13
Danny <dannyconcepcion01@gmail.com>
2020-09-13 23:44:43 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio_3_practica_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3_practica_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+
+            double numero ,suma;
+
+            do
+            {
+
+                Console.WriteLine("Digite un numero");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+            } while (numero  !=0);
+
+            suma = numero + numero;
+
+            Console.WriteLine("El resultado es" + suma);
+
+
+            Console.ReadKey();
+       
+        }
+        
+
+
+    }
+}
